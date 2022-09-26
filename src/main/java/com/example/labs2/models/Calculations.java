bb1a07e0ac82ca75a9eef1154bdbd7f2248ca89a
Anastasiya.Golova <anastasiya.golova@cellpointmobile.com>
2022-09-26 15:36:37 +0400

    add flyway

---
+++ src/main/java/com/example/labs2/models/Calculations.java
@@ -0,0 +1,43 @@
+package com.example.labs2.models;
+
+import com.example.labs2.models.enums.OperationsEnum;
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "calculations")
+public class Calculations {
+    @Id
+    @Column
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "number_one")
+    private String numberOne;
+
+    @NotNull
+    @Column(name = "number_system_one")
+    private Integer numberSystemOne;
+
+    @NotNull
+    @Column(name = "number_two")
+    private String numberTwo;
+
+    @NotNull
+    @Column(name = "number_system_two")
+    private Integer numberSystemTwo;
+
+    @NotNull
+    @Column(name = "date")
+    private Date date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "operation_name")
+    private OperationsEnum operationsEnum;
+}
---
+++ src/main/java/com/example/labs2/models/enums/OperationsEnum.java
@@ -0,0 +1,5 @@
+package com.example.labs2.models.enums;
+
+public enum OperationsEnum {
+    ADDITION, DIVISION, SUBTRACTION, MULTIPLICATION
+}
