cce2669c2b17156452ae855bd3ec00a1d2127bbe
Anastasiya.Golova <anastasiya.golova@cellpointmobile.com>
2022-09-26 17:03:14 +0400

    fix

--- src/main/java/com/example/labs2/models/Numbers.java
+++ src/main/java/com/example/labs2/models/Numbers.java
@@ -8,19 +8,4 @@ import lombok.Setter;
 public class Numbers {
     private int num1;
     private int num2;
-
-    public static int toBinary(int num) {
-        int n = Integer.parseInt(String.valueOf(num), 2);
-        return n;
-    }
-
-    public static int toOctalString(int num) {
-        int n = Integer.parseInt(String.valueOf(num), 8);
-        return n;
-    }
-
-    public static int toHexString(int num) {
-        int n = Integer.parseInt(String.valueOf(num), 16);
-        return n;
-    }
 }
--- src/main/java/com/example/labs2/service/Operations.java
+++ src/main/java/com/example/labs2/service/Operations.java
@@ -9,40 +9,4 @@ public class Operations {
         int res = numbers.getNum1() + numbers.getNum2();
         return res;
     }
-
-    public static int minus(Numbers numbers) {
-        int res = numbers.getNum1() - numbers.getNum2();
-        return res;
-    }
-
-    public static int multiplication(Numbers numbers) {
-        int res = numbers.getNum1() * numbers.getNum2();
-        return res;
-    }
-
-    public static int division(Numbers numbers) {
-        int res = numbers.getNum1() / numbers.getNum2();
-        return res;
-    }
-
-    public static Numbers convert(int num1, int num2, int system) {
-        Numbers numbers = new Numbers();
-        if (system == 2) {
-            numbers.setNum1(Numbers.toBinary(num1));
-            numbers.setNum2(Numbers.toBinary(num2));
-        }
-        if (system == 8) {
-            numbers.setNum1(Numbers.toOctalString(num1));
-            numbers.setNum2(Numbers.toOctalString(num2));
-        }
-        if (system == 16) {
-            numbers.setNum1(Numbers.toHexString(num1));
-            numbers.setNum2(Numbers.toHexString(num2));
-        }
-        if (system == 10) {
-            numbers.setNum1(num1);
-            numbers.setNum2(num2);
-        }
-        return numbers;
-    }
 }
