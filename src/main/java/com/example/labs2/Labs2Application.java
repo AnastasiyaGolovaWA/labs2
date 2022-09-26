5341d9c5f2674a48bf1c69df8aba4c6c57a8d5d8
Anastasiya.Golova <anastasiya.golova@cellpointmobile.com>
2022-09-26 14:44:02 +0400

    Initial commit

---
+++ src/main/java/com/example/labs2/Labs2Application.java
@@ -0,0 +1,13 @@
+package com.example.labs2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labs2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Labs2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/labs2/Labs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labs2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
