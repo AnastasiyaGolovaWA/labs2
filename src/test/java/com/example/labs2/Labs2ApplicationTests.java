479ad1f6a450b95137633af2ae5bb967ca9a20ac
Anastasiya.Golova <anastasiya.golova@cellpointmobile.com>
2022-09-30 18:51:54 +0400

    fix

--- src/test/java/com/example/labs2/Labs2ApplicationTests.java
+++ src/test/java/com/example/labs2/Labs2ApplicationTests.java
@@ -1,13 +1,47 @@
 package com.example.labs2;
 
+import com.example.labs2.repository.CalculationsRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-class Labs2ApplicationTests {
+@ContextConfiguration(initializers = {Labs2ApplicationTests.Initializer.class})
+@Testcontainers
+public class Labs2ApplicationTests {
+    @Autowired
+    CalculationsRepository calculationsRepository;
+    @Container
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:13")
+            .withDatabaseName("numbers")
+            .withUsername("postgres")
+            .withPassword("root");
+            //.withInitScript("resources/db.migration");
 
-	@Test
-	void contextLoads() {
-	}
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                    "spring.datasource.password=" + postgreSQLContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
 
+    @Test
+    @Transactional
+    public void calculatorByParameters() {
+        String hello = "hello";
+        assertEquals("hello", hello);
+    }
 }
