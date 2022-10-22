6ab6789ef4465ad7d9d153a8391a8936b1a9255a
Anastasiya.Golova <Anastasiya.Golova@waveaccess.ru>
2022-10-23 02:25:47 +0400

    Update CucumberIntegrationTest.java

--- src/test/java/com/example/labs2/runner/CucumberIntegrationTest.java
+++ src/test/java/com/example/labs2/runner/CucumberIntegrationTest.java
@@ -1,10 +1,18 @@
 package com.example.labs2.runner;
 
+import com.example.labs2.Labs2Application;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features")
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber-report.html"},
+        glue = { "com.example.labs2.definitions"},
+        features = "src/test/resources/features")
+@CucumberContextConfiguration
+@SpringBootTest(classes = Labs2Application.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class CucumberIntegrationTest {
 }
