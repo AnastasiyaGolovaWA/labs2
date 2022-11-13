f843edc0b98b91809b54de8b2cb76427c8b3a1ae
Anastasiya.Golova <Anastasiya.Golova@waveaccess.ru>
2022-11-14 00:27:01 +0400

    add migration

--- src/main/java/com/example/labs2/Labs2Application.java
+++ src/main/java/com/example/labs2/Labs2Application.java
@@ -5,14 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 @SpringBootApplication
 public class Labs2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Labs2Application.class, args);
-	}
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
+    public static void main(String[] args) throws MalformedURLException {
+        URL url = new URL("http://baeldung.com");
+
+        System.out.println(url.getProtocol());
+        SpringApplication.run(Labs2Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
